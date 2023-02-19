815ddfdb8806a11102f5fb22c808bb001c8914a7
Shubham <shubhamlahase@hmail.com>
2023-02-19 09:42:54 +0530

    added App Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import AppHeader from './Header/AppHeader';
 
 function App() {
   return (
     <div className="App">
       <>
-        
+        <AppHeader/>
       </>
     </div>
   );
---
+++ src/Header/AppHeader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './AppHeader.css';
+
+const AppHeader = () => {
+    return(
+        <>
+            <div className="header">
+                <h1>SnapShot</h1>
+            </div>
+        </>
+    );
+};
+
+export default AppHeader;
